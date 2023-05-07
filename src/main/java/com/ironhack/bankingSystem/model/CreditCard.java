1137b2e7ae3e2d670ec8287166c0394432ce9061
EugeBr <eugebordenave@gmail.com>
2023-05-07 20:44:29 +0200

    added some validations

--- src/main/java/com/ironhack/bankingSystem/model/CreditCard.java
+++ src/main/java/com/ironhack/bankingSystem/model/CreditCard.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -14,4 +16,6 @@ import java.math.BigDecimal;
 public class CreditCard extends Account{
     private BigDecimal creditLimit;
     private Double interestRate;            //? final?
+    @Value("LocalDate.now()")               //? default?
+    private LocalDate lastInterestDate = LocalDate.now();
 }
--- src/main/java/com/ironhack/bankingSystem/model/Savings.java
+++ src/main/java/com/ironhack/bankingSystem/model/Savings.java
@@ -4,9 +4,12 @@ import com.ironhack.bankingSystem.model.enums.Status;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.math.BigDecimal;
 
@@ -16,10 +19,14 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class Savings extends Account{
     private String secretKey;
-    private BigDecimal minimumBalance;      //? final?
+    @Value("1000") //! not sure is correct
+    @DecimalMin("100")
+    private BigDecimal minimumBalance = new BigDecimal(1000);      //? default and min value
     @Enumerated(EnumType.STRING)
     private Status status;
-    private Double interestRate;            //? final?
+    @Value("0.0025")
+    @DecimalMax("0.5")                               //? max value
+    private Double interestRate = 0.0025;           //? default?
 
     public Savings(BigDecimal balance, AccountHolder primaryOwner, String secretKey, Status status) {
         super(balance, primaryOwner);
